5c916e1bbdf1941e201d394ff75b6b090358e2fe
PavaniVemasani18 <vemasani.pavani@gmail.com>
2025-01-28 08:37:15 +0530

    helm

--- run.sh
+++ run.sh
@@ -1,3 +1,6 @@
 source /data/secrets.txt
 cat /data/secrets.txt
+# it indicates to run a Node.js backend in a Docker container
+# starts your application, sets up server configurations, connects to databases, defines routes, and handles incoming requests.
+# (or sometimes app.js or server.js)
 node   /app/index.js
