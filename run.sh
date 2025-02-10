980973698dbfcaf6ad8f1f107e12f775345314e9
PavaniVemasani18 <vemasani.pavani@gmail.com>
2025-02-10 11:43:50 +0530

    rds

--- run.sh
+++ run.sh
@@ -3,4 +3,4 @@ cat /data/secrets.txt
 # it indicates to run a Node.js backend in a Docker container
 # starts your application, sets up server configurations, connects to databases, defines routes, and handles incoming requests.
 # (or sometimes app.js or server.js)
-node   /app/index.js
+node  /app/index.js
