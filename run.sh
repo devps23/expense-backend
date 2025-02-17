1a367c54ac644232fc08046398cc99dc32b39579
PavaniVemasani18 <vemasani.pavani@gmail.com>
2025-02-17 17:48:04 +0530

    backend

--- run.sh
+++ run.sh
@@ -4,15 +4,28 @@ cat /data/secrets.txt
 # starts your application, sets up server configurations, connects to databases, defines routes, and handles incoming requests.
 # (or sometimes app.js or server.js)
 if [ -z "$DB_HOST" ]; then
-  echo DB_HOST is missing
+  echo "DB_HOST not exists"
+  exit 1
+fi
+if [ -z "$RDS_DB_HOST" ]; then
+  echo "RDS_DB_HOST not exists"
   exit 1
 fi
 if [ -z "$RDS_DB_USER" ]; then
-  echo RDS_DB_USER is missing
+  echo "RDS_DB_USER not exists"
   exit 1
 fi
 if [ -z "$RDS_DB_PASSWORD" ]; then
-  echo RDS_DB_PASSWORD is missing
+  echo "RDS_DB_PASSWORD not exists"
   exit 1
 fi
+if [ -z "$component" ]; then
+  echo "component not exists"
+  exit 1
+fi
+if [ -z "$project_name" ]; then
+  echo "project_name not exists"
+  exit 1
+fi
+
 node  /app/index.js
