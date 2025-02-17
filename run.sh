d488628159750a153dbc7f2004917e93f7afaaec
PavaniVemasani18 <vemasani.pavani@gmail.com>
2025-02-17 17:30:50 +0530

    backend

--- run.sh
+++ run.sh
@@ -3,5 +3,16 @@ cat /data/secrets.txt
 # it indicates to run a Node.js backend in a Docker container
 # starts your application, sets up server configurations, connects to databases, defines routes, and handles incoming requests.
 # (or sometimes app.js or server.js)
+if [ -z "$DB_HOST" ]; then
+  echo DB_HOST is missing
+  exit 1
+fi
+if [ -z "$RDS_DB_USER" ]; then
+  echo RDS_DB_USER is missing
+  exit 1
+fi
+if [ -z "$RDS_DB_PASSWORD" ]; then
+  echo RDS_DB_PASSWORD is missing
+  exit 1
+fi
 node  /app/index.js
-echo "Hello"
