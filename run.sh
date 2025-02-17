285f806752974fbcbecb075916cc43c40bd87884
PavaniVemasani18 <vemasani.pavani@gmail.com>
2025-02-17 15:55:39 +0530

    backend

--- run.sh
+++ run.sh
@@ -4,3 +4,4 @@ cat /data/secrets.txt
 # starts your application, sets up server configurations, connects to databases, defines routes, and handles incoming requests.
 # (or sometimes app.js or server.js)
 node  /app/index.js
+echo "Hello"
